97028df07dd9c21df9e88f743af01b71e5a26dbc
shenlink <2024133718@qq.com>
2025-07-02 16:12:00 +0800

    feat(Search): 实现 BinarySearchUpper 二分搜索类
    - 新增 BinarySearchUpper 类，实现获取大于 target 的最小值的索引
    - 添加 BinarySearchUpperTest 测试类，验证 BinarySearchUpper 的功能

---
+++ src/Search/BinarySearchUpper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Shenlink\Algorithms\Search;
+
+/**
+ * upper 二分搜索实现类
+ * 获取大于 target 的最小值的索引
+ */
+class BinarySearchUpper implements BinarySearch
+{
+    /**
+     * 在指定的有序数组中查找大于 target 的最小值的索引。
+     *
+     * @param array<int> $elements 要搜索的有序数组
+     * @param int $target   要查找的目标值
+     * @return int 返回大于 target 的最小值的索引
+     */
+    public function search(array $elements, int $target): int
+    {
+        $left = 0;
+        $right = count($elements);
+        while ($left < $right) {
+            $mid = $left + (int) (($right - $left) / 2);
+            if ($elements[$mid] <= $target) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid;
+            }
+        }
+        return $left;
+    }
+}
